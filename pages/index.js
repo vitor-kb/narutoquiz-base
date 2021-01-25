9577628f75b77ea8003131e6293847b3765c6125
vitor-kb <vitorbassani1@gmail.com>
2021-01-25 18:20:39 -0300

    codigo ao final da primeira aula da Imersão React

---
+++ pages/index.js
@@ -0,0 +1,51 @@
+import styled from 'styled-components'
+import db from '../db.json'
+import Widget from '../src/components/Widget'
+import Footer from '../src/components/Footer'
+import GitHubCorner from '../src/components/GitHubCorner'
+import QuizBackground from '../src/components/QuizBackground'
+
+  // const BackgroundImage = styled.div` //outro jeito de usar uma function
+  // background-image: url(${db.bg});
+  // flex: 1;
+  // background-size: cover;
+  // background-position: center;
+// `;
+
+  export const QuizContainer =  styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px){
+    margin: auto;
+    padding: 15px;
+  }
+`;
+
+export default function Home() {
+  return (
+    <QuizBackground backgroundImage={db.bg}>
+      <QuizContainer>
+        <Widget>
+          <Widget.Header>
+            <h1>Naruto</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <p>lorem ipsum dolor sit amet...</p>
+          </Widget.Content>
+        </Widget>
+
+        <Widget>
+          <Widget.Content>
+            <h1>Quizes da galera</h1>
+
+            <p>lorem ipsum dolor sit amet...</p>
+          </Widget.Content>
+        </Widget>
+        <Footer/>
+      </QuizContainer>
+      <GitHubCorner projectUrl="https://github.com/vitor-kb"/>
+    </QuizBackground>
+  );
+}
